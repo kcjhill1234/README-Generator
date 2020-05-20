761ceb2a1b28e5a3b88ad1a0741ce0cde5b12edc
ktotheH <kcjhill1234@msn.com>
2020-05-20 16:57:42 -0600

    files

---
+++ index.js
@@ -0,0 +1,19 @@
+const questions = [{
+    type: "input",
+    name: "title",
+    message: "please enter a title",
+    default: "Default Readme",
+    
+
+}
+
+];
+
+function writeToFile(fileName, data) {
+}
+
+function init() {
+
+}
+
+init();
