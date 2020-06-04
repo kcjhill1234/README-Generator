7579ba9b76e670ae484a618eaf224c63813e4db0
ktotheH <kcjhill1234@msn.com>
2020-06-04 15:55:22 -0600

    Add questions and refactor

--- index.js
+++ index.js
@@ -3,54 +3,63 @@ const inquirer = require("inquirer");
 const generateMarkdown = require("./utils/generateMarkdown");
 const writeFile = require("./utils/writeFile");
 
-const questions = [
-  {
-    type: "input",
-    name: "title",
-    message: "Please enter a title.",
-    default: "Project",
-  },
-  {
-    type: "input",
-    name: "description",
-    message: "Please enter a description",
-    default: "TODO write description",
-  },
-  {
-    type: "input",
-    name: "installation",
-    message: "Please enter instructions",
-  },
-  {
-    type: "input",
-    name: "usage",
-    message: "Please enter how to use project",
-  },
-  {
-    type: "input",
-    name: "license",
-    message: "Please enter license information",
-  },
-  {
-    type: "input",
-    name: "contributing",
-    message: "Please enter contributors",
-  },
-  {
-    type: "input",
-    name: "tests",
-    message: "Please enter test information",
-  },
-];
-
-async function writeToFile(fileName, data) {
-  const filepath = path.resolve(__dirname, "dist", fileName);
-  await writeFile(filepath, data);
+const questions = [{
+  type: "input",
+  name: "title",
+  message: "Please enter a title.",
+  default: "Project",
+}, {
+  type: "input",
+  name: "description",
+  message: "Please enter a description",
+  default: "TODO write description",
+}, {
+  type: "confirm",
+  name: "installation",
+  message: "Do you want an installation section?",
+  default: false
+}, {
+  type: "confirm",
+  name: "usage",
+  message: "Do you want a usage section?",
+  default: false
+}, {
+  type: "confirm",
+  name: "license",
+  message: "Do you want a license section?",
+  default: false
+}, {
+  type: "confirm",
+  name: "contributing",
+  message: "Do you want a contributors section?",
+  default: false
+}, {
+  type: "confirm",
+  name: "tests",
+  message: "Do you want a tests section?",
+  default: false
+}, {
+  type: 'confirm',
+  name: 'tableOfContents',
+  message: 'Do you want a table of contents',
+  default: false,
+}, {
+  type: 'input',
+  name: 'username',
+  message: 'Please enter username',
+}, {
+  type: 'input',
+  name: 'repository',
+  message: 'Please enter name of repository',
 }
+];
 
 async function init() {
   const answers = await inquirer.prompt(questions);
-  await writeToFile("README.md", generateMarkdown(answers));
+  const filepath = path.resolve(__dirname, "dist", "README.md");
+  await writeFile(filepath, generateMarkdown(answers));
+
+  console.log('\n\nYour README was generated at', filepath);
 }
 
 init();
